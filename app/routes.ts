5631f72aceb03791e12009aa23dd3ef3420f3e97
Henners-111 <76429015+Hotdog-111@users.noreply.github.com>
2025-06-20 20:24:13 +0100

    new project!

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,12 @@
 import type { RouteConfig } from "@react-router/dev/routes";
+import { 
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
-export default [] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("contacts/:contactId", "routes/contact.tsx"),
+  route("about", "routes/about.tsx"),
+] satisfies RouteConfig;
